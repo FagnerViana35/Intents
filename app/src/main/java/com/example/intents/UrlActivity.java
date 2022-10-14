c97ddedea831ac2d442db7b8bb890619b272de25
Fagner Viana <fagnerviana03@gmail.com>
2022-10-13 22:52:57 -0300

    Foi adicionado

--- app/src/main/java/com/example/intents/UrlActivity.java
+++ app/src/main/java/com/example/intents/UrlActivity.java
@@ -1,6 +1,39 @@
 package com.example.intents;
 
-import android.app.Activity;
+import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+import com.example.intents.databinding.ActivityUrlBinding;
 
-public class UrlActivity extends Activity {
+class UrlActivity : AppCompatActivity() {
+
+    private val aub: ActivityUrlBinding by lazy{
+        ActivityUrlBinding.inflate(layoutInflater)
+        }
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(aub.root)
+
+        supportActionBar?.subtitle = "UrlActivity"
+
+        val urlAnterior = intent.getStringExtra("URL") ?: ""
+
+        if (urlAnterior.isNotEmpty()){
+        aub.urlEt.setText(urlAnterior)
+        }
+
+        Toast.makeText(this, "Estudo Intents",Toast.LENGTH_SHORT).show()
+
+        aub.entrarUrlBt.setOnClickListener {
+        val retornoIntent = Intent()
+
+        retornoIntent.putExtra("URL", aub.urlEt.text.toString())
+
+        setResult(RESULT_OK, retornoIntent)
+        finish()
+        }
+
+    }
 }
