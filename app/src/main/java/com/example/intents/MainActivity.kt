1c1ccc90074f397c170f27dedc2bd9fda0eacce9
Fagner Viana <fagnerviana03@gmail.com>
2022-10-13 22:36:59 -0300

    Foi adicionado o ActivityMainBinding

--- app/src/main/java/com/example/intents/MainActivity.kt
+++ app/src/main/java/com/example/intents/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.intents
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.ActivityResultLauncher
+import com.example.intents.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val amb: ActivityMainBinding by lazy{
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private lateinit var urlArl: ActivityResultLauncher<Intent>
+    private lateinit var pegarImagemArl: ActivityResultLauncher<Intent>
+    private lateinit var permissaoChamadaArl: ActivityResultLauncher<String>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(amb.root)
+
+        supportActionBar?.subtitle = "MainActivity"
     }
 }
